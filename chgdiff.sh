fde76b5b96c451d01e4cdc83a991d42baf972da4
liuzh1206 <2311531808@qq.com>
2024-05-21 10:29:48 +0800

    add date

--- chgdiff.sh
+++ chgdiff.sh
@@ -4,22 +4,18 @@ for i in `seq 0.05 0.05 0.40`
 do {
     cd $i
 
-    vaspkit -task 426 -dim 3
     echo -e "314\nCHGCAR ../../$A/CHGCAR" `echo ../../$B*`'/CHGCAR' | vaspkit
-    mv PLANAR_AVERAGE.dat PLANAR_AVERAGE.dat.old
     vaspkit -task 316 -file CHGDIFF.vasp -dim 3
-    mv PLANAR_AVERAGE.dat CHARGE_PLANAR_AVERAGE.dat
-    mv PLANAR_AVERAGE.dat.old PLANAR_AVERAGE.dat
+    mv PLANAR_AVERAGE.dat PLANAR_AVERAGE_CHARGE.dat
+    vaspkit -task 426 -dim 3
 
     cd ../
     cd n$i
 
-    vaspkit -task 426 -dim 3
     echo -e "314\nCHGCAR ../../$A/CHGCAR" `echo ../../$B*`'/CHGCAR' | vaspkit
-    mv PLANAR_AVERAGE.dat PLANAR_AVERAGE.dat.old
     vaspkit -task 316 -file CHGDIFF.vasp -dim 3
-    mv PLANAR_AVERAGE.dat CHARGE_PLANAR_AVERAGE.dat
-    mv PLANAR_AVERAGE.dat.old PLANAR_AVERAGE.dat
+    mv PLANAR_AVERAGE.dat PLANAR_AVERAGE_CHARGE.dat
+    vaspkit -task 426 -dim 3
 
     cd ../
 } &
