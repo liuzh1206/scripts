9397bc16a7cdccb044695d8e5bc0fa27f455b0aa
liuzh1206 <2311531808@qq.com>
2024-05-26 20:53:17 +0800

    update tar ueage way

--- Etar.sh
+++ Etar.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-tar -zcvf E.tar.gz E --exclude WAVECAR --exclude CHG --exclude CHGCAR --exclude AECCAR0 --exclude AECCAR1 --exclude AECCAR2 --exclude vaspout.h5 --exclude vasprun.xml --exclude POT --exclude LOCPOT
+tar -zcvf E.tar.gz --exclude WAVECAR --exclude CHG --exclude CHGCAR --exclude AECCAR0 --exclude AECCAR1 --exclude AECCAR2 --exclude vaspout.h5 --exclude vasprun.xml --exclude POT --exclude LOCPOT E
--- banddos.sh
+++ banddos.sh
@@ -1,2 +1,2 @@
 #!/bin/env bash
-tar -zcvf banddos.tar.gz band dos --exclude WAVECAR --exclude CHG --exclude CHGCAR --exclude AECCAR0 --exclude AECCAR1 --exclude AECCAR2 --exclude vasprun.xml --exclude POT --exclude LOCPOT
+tar -zcvf banddos.tar.gz --exclude WAVECAR --exclude CHG --exclude CHGCAR --exclude AECCAR0 --exclude AECCAR1 --exclude AECCAR2 --exclude vasprun.xml --exclude POT --exclude LOCPOT band dos
